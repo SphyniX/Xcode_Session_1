ee7d3d8cca38ce2671549638f193456b95a0b68c
RicherTecMac Pro <js421306615@live.cn>
2016-09-09 12:04:59 +0800

    Building Starting

---
+++ Xcode_Session_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xcode_Session_1
+//
+//  Created by RicherTecMac Pro on 2016/9/9.
+//  Copyright © 2016年 KJSA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
